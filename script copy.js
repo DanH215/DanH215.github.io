6a21749e14ac708a4fa307d44128b7a04fdbdf71
DanH215 <dh215@hotmail.co.uk>
2023-06-28 20:19:52 +0100

    most recent changes

--- script copy.js
+++ script copy.js
@@ -41,7 +41,7 @@ const ArtList = [
     },
     {
         name: "memento mori, SIZE, acrylic on paper",
-        image: "assets/A7_08871_ty.jpeg",
+        image: "assets/WhatsApp1.jpeg",
     },
     {
         name: "from all she surveys, 50cm x 65cm, acrylic on paper",
@@ -243,254 +243,7 @@ const ArtList = [
         image: "assets/A7_08909_web_adjusted.jpg"
     }
 ]
-// const ArtList = [
-//     {
-//         name: "8 windows to the end, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08784_web_adjusted.jpg",
-//     },
-//     {
-//         name: "24 hills, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08786_web_adjusted - rotate anticlockwise.jpg",
-//     },
-//     {
-//         name: "8 windows with stripes, YEAR, 60cm x 90cm, acrylic on canvas",
-//         image: "assets/A7_08807_web_adjusted - rotated anticlockwise.jpg"
-//     },
-//     {
-//         name: "8 windos with apples and snails, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08788 - rotate clockwise.JPG",
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08830_web_adjusted - rotate clockwise.jpg",
-//     },
-//     {
-//         name: "we belong together, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08838_web_adjusted - rotate clockwise.jpg",
-//     },
-//     {
-//         name: "magenta drive #2, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08916_web_adjusted.jpg"
-//     },
-//     {
-//         name: "magenta drive #1, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08914_web_adjusted rotate clockwise.jpg",
-//     },
-//     {
-//         name: "magenta drive #3, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08918_web_adjusted.jpg"
-//     },
 
-//     {
-//         name: "exercise in form and colour #2, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08895_web_adjusted.jpg"
-//     },
-//     {
-//         name: "memento mori, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08871_ty.jpeg",
-//     },
-//     {
-//         name: "from all she surveys, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08890_web_adjusted.jpg"
-//     },
-//     {
-//         name: "sundown with the petal collectors, YEAR, 85cm x 60cm, acrylic on paper",
-//         image: "assets/A7_08869_web_uncorrected.jpg",
-//     },
-//     {
-//         name: "fecundity, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08873_web_adjusted.jpg"
-//     },
-//     {
-//         name: "exercise in form and colour #1, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08902_web_adjusted - rotate clockwise.jpg",
-//     },
-//     {
-//         name: "8 windows, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08907_web_adjusted - rotate antioclockwise.jpg",
-//     },
-//     {
-//         name: "the plank, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08912_1_web_adjusted_2 - rotate anticlockwise.jpg",
-//     },
-//     {
-//         name: "welcome to the world famous nothing 2 declare, 2023, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08782_web_adjusted.jpg",
-//     },
-//     {
-//         name: "the black ragged crow, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08791_web_adjusted.jpg",
-//     },
-//     {
-//         name: "self portrait with 8 windows, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08798_web_adjusted.jpg",
-//     },
-//     {
-//         name: "stella, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08800_web_adjusted.jpg",
-//     },
-//     {
-//         name: "the vote, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08803_web_adjusted.jpg"
-//     },
-//     {
-//         name: "mountain goats called to ascend, YEAR, 60cm x 80cm, acrylic on canvas",
-//         image: "assets/A7_08805_web_adjusted.jpg"
-//     },
-//     {
-//         name: "wood from the trees, YEAR, SIZE, acrylic on canvas",
-//         image: "assets/A7_08811_web_adjusted.jpg"
-//     },
-//     {
-//         name: "boys and girls in the city, YEAR, 100cm x 100cm, acrylic on canvas",
-//         image: "assets/A7_08815_web_adjusted.jpg"
-//     },
-//     {
-//         name: "since god was a boy, YEAR, 100cm x 100cm, acrylic on canvas",
-//         image: "assets/A7_08818_web_adjusted.jpg"
-//     },
-//     {
-//         name: "untitled, YEAR, 100cm x 100cm, acrylic on canvas",
-//         image: "assets/A7_08824_web_adjusted.jpg"
-//     },
-//     {
-//         name: "on bukowskis' bluebird, YEAR, 100cm x 100cm, acrylic on canvas",
-//         image: "assets/A7_08826_web_adjusted.jpg"
-//     },
-//     {
-//         name: "landscape and sky, YEAR, 100cm x 100cm, acrylic on canvas",
-//         image: "assets/A7_08828_web_adjusted.jpg"
-//     },
-//     {
-//         name: "what did you expect?, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08833_web_adjusted.jpg"
-//     },
-//     {
-//         name: "as we were, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08835_web_adjusted.jpg"},
-//     {
-//         name: "some windows, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08837_web_adjusted.jpg"
-//     },
-//     {
-//         name: "this is water: on david foster wallace, YEAR, 50cm x 65cm, acrylic on canvas",
-//         image: "assets/A7_08840_web_adjusted.jpg"
-//     },
-//     {
-//         name: "still life with teeth, YEAR, 50cm x 65cm, acrylic on canvas",
-//         image: "assets/A7_08842_web_adjusted.jpg"
-//     },
-//     {
-//         name: "on matisse, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08844_web_adjusted.jpg"
-//     },
-//     {
-//         name: "on bridget, YEAR, 50cm x 650cm, acrylic on paper",
-//         image: "assets/A7_08846_web_adjusted.jpg"
-//     },
-//     {
-//         name: "esse est percipi - there will be blood, YEAR, 50cm x 65cm, acrylic on canvas",
-//         image: "assets/A7_08848_web_adjusted.jpg"
-//     },
-//     {
-//         name: "unrequited love, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08850_web_adjusted.jpg"
-//     },
-//     {
-//         name: '"to the stairway!", YEAR, 50cm x 65cm, acrylic on paper',
-//         image: "assets/A7_08852_web_adjusted.jpg"
-//     },
-//     {
-//         name: "the blue window, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08854_web_adjusted.jpg"
-//     },
-//     {
-//         name: "jus slide, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08856_web_adjusted.jpg"
-//     },
-//     {
-//         name: "late dinner, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08858_web_adjusted.jpg"
-//     },
-//     {
-//         name: "on van gogh, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08860_web_adjusted.jpg"
-//     },
-//     {
-//         name: "hmmmm, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08862_web_adjusted.jpg"
-//     },
-//     {
-//         name: "death and the maiden, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08864_web_adjusted.jpg"
-//     },
-//     {
-//         name: "the land of unlikeliness, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08866_web_adjusted.jpg"
-//     },
-//     {
-//         name: "i wouldn't go that far, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08893_web_adjusted - rotate anticlockwise.jpg"
-//     },
-//     {
-//         name: "untitled, YEAR, 50cm x 65cm, acrylic on paper",
-//         image: "assets/A7_08897_web_adjusted.jpg"
-//     },
-//     {
-//         name: "8 windows, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08899_web_adjusted.jpg"
-//     },
-//     {
-//         name: "the dance, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08904_web_adjusted.jpg"
-//     },
-//     {
-//         name: "8 windows, YEAR, SIZE, acrylic on paper",
-//         image: "assets/8 windows.jpeg"
-//     },
-//     {
-//         name: "the star of the show is always an anvil., YEAR, SIZE, acrylic on paper",
-//         image: "assets/the star of the show is always an anvil.jpeg"
-//     },
-//     {
-//         name: "30 hills, YEAR, SIZE, acrylic on paper",
-//         image: "assets/30 hills.jpeg"
-//     },
-//     {
-//         name: "behind the green door., YEAR, SIZE, acrylic on paper",
-//         image: "assets/Behind the green door.jpeg"
-//     },
-//     {
-//         name: "on radioheads' weird fishes, YEAR, SIZE, acrylic on paper",
-//         image: "assets/On radioheads' weird fishes.jpeg"
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/WhatsApp1.jpeg"
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/WhatsApp Image 2023-05-19 at 18-32-51.jpeg"
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/WhatsApp Image 2023-05-19 at 18-38-08.jpeg"
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/WhatsApp Image 2023-05-19 at 18.38.27.jpeg"
-//     },
-//     {
-//         name: "untitled, YEAR, SIZE, acrylic on paper",
-//         image: "assets/WhatsApp Image 2023-05-19 at 18.41.16.jpeg"
-//     },
-//     {
-//         name: "the descent of icarus, YEAR, SIZE, acrylic on paper",
-//         image: "assets/A7_08909_web_adjusted.jpg"
-//     }
-// ]
-// const gallery = document.getElementsByClassName('gallery')
-// const column = document.getElementsByClassName('column')
 function AddImage(Array, CardIndex, ArrayIndex, columnNumber) 
 {
     if (columnNumber==0) {
@@ -504,16 +257,19 @@ function AddImage(Array, CardIndex, ArrayIndex, columnNumber)
     CardId = ColumnId + " Card " + CardIndex
     Card.id = CardId
     column.append(Card)
-    const Break = document.createElement('br')
-    column.append(Break)
+    for (let index = 0; index < 10; index++) {
+        const Break = document.createElement('br')
+        column.append(Break)
+    }
+
+    const ImageToAdd = document.createElement("img");
+    ImageToAdd.setAttribute("src", Array[ArrayIndex].image);
+    ImageToAdd.classList.add("card-img-bottom");
 
     const CardText = document.createElement("p");
     CardText.classList.add("card-text")
     CardText.innerText = Array[ArrayIndex].name
     
-    const ImageToAdd = document.createElement("img");
-    ImageToAdd.setAttribute("src", Array[ArrayIndex].image);
-    ImageToAdd.classList.add("card-img-top");
 
     ImageToAdd.addEventListener('click', function() {
         const imageUrl = ImageToAdd.src;
