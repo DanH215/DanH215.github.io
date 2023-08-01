57151c07930cd2c8a6e4cf47e7e0efbcf51d4e41
DanH215 <dh215@hotmail.co.uk>
2023-08-01 14:56:06 +0100

    01/08/23

--- script copy.js
+++ script copy.js
@@ -25,55 +25,55 @@ const ArtList = [
         image: "assets/A7_08788.png",
     },
     {
-        name: "untitled, SIZE, acrylic on paper",
+        name: "untitled, SIZE, acrylic on canvas paper",
         image: "assets/A7_08830.png",
     },
     {
-        name: "we belong together, 50cm x 65cm, acrylic on paper",
+        name: "we belong together, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08838.png",
     },
+    // {
+    //     name: "magenta drive #2, 50cm x 65cm, acrylic on paper",
+    //     image: "assets/A7_08916_web_adjusted.jpg"
+    // },
+    // {
+    //     name: "magenta drive #1, SIZE, acrylic on paper",
+    //     image: "assets/A7_08914.png",
+    // },
+    // {
+    //     name: "magenta drive #3, 50cm x 65cm, acrylic on paper",
+    //     image: "assets/A7_08918_web_adjusted.jpg"
+    // },
+    // {
+    //     name: "exercise in form and colour #2, 50cm x 65cm, acrylic on paper",
+    //     image: "assets/A7_08895_web_adjusted.jpg"
+    // },
+    // {
+    //     name: "memento mori, SIZE, acrylic on paper",
+    //     image: "assets/WhatsApp1.jpeg",
+    // },
+    // {
+    //     name: "from all she surveys, 50cm x 65cm, acrylic on paper",
+    //     image: "assets/A7_08890_web_adjusted.jpg"
+    // },
     {
-        name: "magenta drive #2, 50cm x 65cm, acrylic on paper",
-        image: "assets/A7_08916_web_adjusted.jpg"
-    },
-    {
-        name: "magenta drive #1, SIZE, acrylic on paper",
-        image: "assets/A7_08914.png",
-    },
-    {
-        name: "magenta drive #3, 50cm x 65cm, acrylic on paper",
-        image: "assets/A7_08918_web_adjusted.jpg"
-    },
-    {
-        name: "exercise in form and colour #2, 50cm x 65cm, acrylic on paper",
-        image: "assets/A7_08895_web_adjusted.jpg"
-    },
-    {
-        name: "memento mori, SIZE, acrylic on paper",
-        image: "assets/WhatsApp1.jpeg",
-    },
-    {
-        name: "from all she surveys, 50cm x 65cm, acrylic on paper",
-        image: "assets/A7_08890_web_adjusted.jpg"
-    },
-    {
-        name: "sundown with the petal collectors, 85cm x 60cm, acrylic on paper",
+        name: "sundown with the petal collectors, 85cm x 60cm, acrylic on canvas paper",
         image: "assets/A7_08869_web_uncorrected.jpg",
     },
     {
-        name: "fecundity, 50cm x 65cm, acrylic on paper",
+        name: "fecundity, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08873_web_adjusted.jpg"
     },
     {
-        name: "exercise in form and colour #1, SIZE, acrylic on paper",
+        name: "exercise in form and colour #1, SIZE, acrylic on canvas paper",
         image: "assets/A7_08902.png",
     },
     {
-        name: "8 windows, SIZE, acrylic on paper",
+        name: "8 windows, SIZE, acrylic on canvas paper",
         image: "assets/A7_08907.png",
     },
     {
-        name: "the plank, SIZE, acrylic on paper",
+        name: "the plank, SIZE, acrylic on canvas paper",
         image: "assets/A7_08912.png",
     },
     {
@@ -125,114 +125,114 @@ const ArtList = [
         image: "assets/A7_08828_web_adjusted.jpg"
     },
     {
-        name: "what did you expect?, 50cm x 65cm, acrylic on paper",
+        name: "what did you expect?, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08833_web_adjusted.jpg"
     },
     {
-        name: "as we were, SIZE, acrylic on paper",
+        name: "when we were, SIZE, acrylic on canvas paper",
         image: "assets/A7_08835_web_adjusted.jpg"},
     {
-        name: "some windows, 50cm x 65cm, acrylic on paper",
+        name: "some windows, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08837_web_adjusted.jpg"
     },
     {
-        name: "this is water: on david foster wallace, 50cm x 65cm, acrylic on canvas",
+        name: "this is water: on david foster wallace, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08840_web_adjusted.jpg"
     },
     {
-        name: "still life with teeth, 50cm x 65cm, acrylic on canvas",
+        name: "still life with teeth, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08842_web_adjusted.jpg"
     },
     {
-        name: "on matisse, 50cm x 65cm, acrylic on paper",
+        name: "on matisse, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08844_web_adjusted.jpg"
     },
     {
-        name: "on bridget, 50cm x 650cm, acrylic on paper",
+        name: "on bridget, 50cm x 650cm, acrylic on canvas paper",
         image: "assets/A7_08846_web_adjusted.jpg"
     },
     {
-        name: "esse est percipi - there will be blood, 50cm x 65cm, acrylic on canvas",
+        name: "esse est percipi - there will be blood, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08848_web_adjusted.jpg"
     },
     {
-        name: "unrequited love, 50cm x 65cm, acrylic on paper",
+        name: "unrequited love, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08850_web_adjusted.jpg"
     },
     {
-        name: '"to the stairway!", 50cm x 65cm, acrylic on paper',
+        name: '"to the stairway!", 50cm x 65cm, acrylic on canvas paper',
         image: "assets/A7_08852_web_adjusted.jpg"
     },
     {
-        name: "the blue window, 50cm x 65cm, acrylic on paper",
+        name: "the blue window, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08854_web_adjusted.jpg"
     },
     {
-        name: "jus slide, SIZE, acrylic on paper",
+        name: "jus slide, SIZE, acrylic on canvas paper",
         image: "assets/A7_08856_web_adjusted.jpg"
     },
     {
-        name: "late dinner, 50cm x 65cm, acrylic on paper",
+        name: "late dinner, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08858_web_adjusted.jpg"
     },
     {
-        name: "on van gogh, 50cm x 65cm, acrylic on paper",
+        name: "on van gogh, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08860_web_adjusted.jpg"
     },
     {
-        name: "hmmmm, 50cm x 65cm, acrylic on paper",
+        name: "hmmmm, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08862_web_adjusted.jpg"
     },
     {
-        name: "death and the maiden, 50cm x 65cm, acrylic on paper",
+        name: "death and the maiden, 50cm x 65cm, acrylic on canvas paper",
         image: "assets/A7_08864.png"
     },
     {
-        name: "the land of unlikeliness, SIZE, acrylic on paper",
+        name: "the land of unlikeliness, SIZE, acrylic on canvas paper",
         image: "assets/A7_08866_web_adjusted.jpg"
     },
     {
-        name: "i wouldn't go that far, SIZE, acrylic on paper",
+        name: "i wouldn't go that far, SIZE, acrylic on canvas paper",
         image: "assets/A7_08893.png"
     },
     {
-        name: "untitled, 50cm x 65cm, acrylic on paper",
+        name: "fuk art let's dance, 50cm x 65cm, acrylic on paper",
         image: "assets/A7_08897_web_adjusted.jpg"
     },
     {
-        name: "8 windows, SIZE, acrylic on paper",
+        name: "8 windows, SIZE, acrylic on canvas paper",
         image: "assets/A7_08899_web_adjusted.jpg"
     },
     {
-        name: "the dance, SIZE, acrylic on paper",
+        name: "the dance, SIZE, acrylic on canvas paper",
         image: "assets/A7_08904_web_adjusted.jpg"
     },
     {
-        name: "8 windows, SIZE, acrylic on paper",
+        name: "8 windows, 60cm x 80cm, acrylic on canvas",
         image: "assets/8 windows.jpeg"
     },
     {
-        name: "the star of the show is always an anvil., SIZE, acrylic on paper",
+        name: "the star of the show is always an anvil., SIZE, acrylic on canvas",
         image: "assets/the star of the show is always an anvil.jpeg"
     },
     {
-        name: "30 hills, SIZE, acrylic on paper",
+        name: "30 hills, 60cm x 80cm, acrylic on canvas",
         image: "assets/30 hills.jpeg"
     },
     {
-        name: "behind the green door., SIZE, acrylic on paper",
+        name: "behind the green door., SIZE, acrylic on canvas",
         image: "assets/Behind the green door.jpeg"
     },
     {
-        name: "on radioheads' weird fishes, SIZE, acrylic on paper",
-        image: "assets/On radioheads' weird fishes.jpeg"
+        name: "on radioheads' weird fishes, SIZE, acrylic on canvas",
+        image: "assets/WhatsApp Image 2023-08-01 at 14.12.47.jpeg"
     },
     {
         name: "untitled, SIZE, acrylic on paper",
         image: "assets/WhatsApp1.jpeg"
     },
     {
-        name: "untitled, SIZE, acrylic on paper",
+        name: "untitled, SIZE, acrylic on canvas",
         image: "assets/WhatsApp Image 2023-05-19 at 18-32-51.jpeg"
     },
     {
@@ -244,11 +244,11 @@ const ArtList = [
         image: "assets/WhatsApp Image 2023-05-19 at 18.38.27.jpeg"
     },
     {
-        name: "untitled, SIZE, acrylic on paper",
+        name: "the dog's small black old rubber ball, SIZE, acrylic on canvas",
         image: "assets/WhatsApp Image 2023-05-19 at 18.41.16.jpeg"
     },
     {
-        name: "the descent of icarus, SIZE, acrylic on paper",
+        name: "the descent of icarus, SIZE, acrylic on canvas paper",
         image: "assets/A7_08909_web_adjusted.jpg"
     }
 ]
